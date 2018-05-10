d16157fb502c3d2d75b9a54bbcf10a4b282a6bc1
Greg Burns <colburnside@hotmail.com>
2018-05-10 07:51:06 -0400

    fixed warnings

---
+++ ParseWorkoutData/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SaveFileDialog2 = New System.Windows.Forms.SaveFileDialog()
+        Me.SaveFileDialog3 = New System.Windows.Forms.SaveFileDialog()
+        Me.SaveFileDialog4 = New System.Windows.Forms.SaveFileDialog()
+        Me.txtDataToParse = New System.Windows.Forms.TextBox()
+        Me.btnParse = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtDataToParse
+        '
+        Me.txtDataToParse.AllowDrop = True
+        Me.txtDataToParse.Font = New System.Drawing.Font("Courier New", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDataToParse.Location = New System.Drawing.Point(12, 12)
+        Me.txtDataToParse.Multiline = True
+        Me.txtDataToParse.Name = "txtDataToParse"
+        Me.txtDataToParse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDataToParse.Size = New System.Drawing.Size(766, 218)
+        Me.txtDataToParse.TabIndex = 0
+        '
+        'btnParse
+        '
+        Me.btnParse.Enabled = False
+        Me.btnParse.Location = New System.Drawing.Point(12, 246)
+        Me.btnParse.Name = "btnParse"
+        Me.btnParse.Size = New System.Drawing.Size(193, 48)
+        Me.btnParse.TabIndex = 1
+        Me.btnParse.Text = "Parse"
+        Me.btnParse.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnParse)
+        Me.Controls.Add(Me.txtDataToParse)
+        Me.Name = "Form1"
+        Me.Text = "Parse Workout Data"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents SaveFileDialog2 As SaveFileDialog
+    Friend WithEvents SaveFileDialog3 As SaveFileDialog
+    Friend WithEvents SaveFileDialog4 As SaveFileDialog
+    Friend WithEvents txtDataToParse As TextBox
+    Friend WithEvents btnParse As Button
+End Class
---
+++ ParseWorkoutData/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtDataToParse.TextChanged
+        btnParse.Enabled = True
+    End Sub
+
+    Private Sub btnParse_Click(sender As Object, e As EventArgs) Handles btnParse.Click
+        Dim dateLine As String
+        Dim workoutDate As Date
+        Dim spaceAfterDay As Integer
+        Dim workoutDay As Integer
+
+        For Each strLine As String In txtDataToParse.Text.Split(vbNewLine)
+            If InStr(strLine, "FitNotes Workout - ") Then
+                dateLine = strLine.Substring(19, Len(strLine) - 19)
+                spaceAfterDay = InStr(1, dateLine, " ")
+                dateLine = dateLine.Substring(spaceAfterDay, Len(dateLine) - (spaceAfterDay))
+                MsgBox(dateLine)
+                'dateLine = "30 April 2018"
+                'workoutDay = Convert.ToInt32(dateLine)
+                'MsgBox(workoutDay)
+                workoutDate = CDate(dateLine)
+            End If
+            Debug.Print(strLine)
+        Next
+        MsgBox(workoutDate)
+    End Sub
+End Class
